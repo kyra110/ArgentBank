f019c106a3a8a23755ad9d84ae5c37bcb933507f
Michel <michel.alex.partenaire@gmail.com>
2024-04-01 13:08:04 +0200

    Ajout d'une condition qui vérifie si nous sommes connectés avant toute éventuelle redirection vers la page sign-in dans l'URL, ainsi que la modification du Swagger avec les corrections nécessaires pour la phase 2.

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -2,26 +2,58 @@ import Header from "./components/Header/Header.jsx";
 import MainHome from "./components/MainHome/MainHome.jsx";
 import Footer from "./components/Footer/Footer.jsx";
 import SignIn from "./components/SignIn/SignIn.jsx";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import User from "./components/User/User.jsx";
 import EditName from "./components/EditName/EditName.jsx";
 import SecurityRoutes from "./SecurityRoutes.jsx";
-
+import { useSelector } from "react-redux";
 
 
 const App = () => {
-const  basename= import.meta.env.MODE  === "production" ? "/ArgentBank/" : "/";
+  const basename = import.meta.env.MODE === "production" ? "/ArgentBank/" : "/";
+
+  const userProfil = useSelector(state =>state.login.userProfil)
+  // Fonction pour effectuer la redirection si l'utilisateur est déjà connecté
+  const redirectIfLoggedIn = () => {
+    console.log("valleur du token " +  userProfil);
+    if (userProfil) {
+      return <Navigate to="/user" />;
+    }
+    return null;
+  };
 
   return (
     <BrowserRouter basename={basename}>
-    <Header/>
-    <Routes>
-    <Route path="/" element={<MainHome/>}/>
-    <Route path="/sign-in" element={<SignIn/>}/>
-    <Route path="/user" element={<SecurityRoutes><User/></SecurityRoutes>}/>
-    <Route path="/editUser" element={<SecurityRoutes><EditName/></SecurityRoutes>}/>
-    </Routes>
-    <Footer/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainHome />} />
+         <Route
+          path="/sign-in"
+          element={
+            <>
+              {redirectIfLoggedIn()}
+              <SignIn/>
+            </>
+          }
+        />
+        <Route
+          path="/user"
+          element={
+            <SecurityRoutes>
+              <User />
+            </SecurityRoutes>
+          }
+        />
+        <Route
+          path="/editUser"
+          element={
+            <SecurityRoutes>
+              <EditName />
+            </SecurityRoutes>
+          }
+        />
+      </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
